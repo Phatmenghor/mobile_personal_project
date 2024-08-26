8fdabefcfa53c92b286bb2d2e119dc92e25ea20e
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-08-26 09:49:21 +0700

    add dev

---
+++ web/firebase/production/firebase-config.js
@@ -0,0 +1,12 @@
+// web/firebase/staging/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAs0U6jsFtNAZ2EnbOY7Whqia4sBD1w8PM",
+  appId: "1:298022819695:web:17d925d994897fedc2fa9a",
+  messagingSenderId: "298022819695",
+  projectId: "learning-production-a10d2",
+  authDomain: "learning-production-a10d2.firebaseapp.com",
+  storageBucket: "learning-production-a10d2.appspot.com",
+  measurementId: "G-WLY23TXVNT",
+};
+
+export default firebaseConfig;
---
+++ web/firebase/staging/firebase-config.js
@@ -0,0 +1,12 @@
+// web/firebase/staging/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBsMitT2N7aMpw7ubjFAu3LnolDh4Mrar0",
+  appId: "1:766346609290:web:6d41214c694c2712356373",
+  messagingSenderId: "766346609290",
+  projectId: "learning-staging-3ca9f",
+  authDomain: "learning-staging-3ca9f.firebaseapp.com",
+  storageBucket: "learning-staging-3ca9f.appspot.com",
+  measurementId: "G-9ENFKM07K0",
+};
+
+export default firebaseConfig;
